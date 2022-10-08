30cf39df0eb5a539239f2c740cb3b898b03f5e76
Lhotte Louis <louis.lhotte@student-cs.fr>
2022-10-08 13:48:14 +0200

    critère isprime

--- app1/src/prime_numbers.py
+++ app1/src/prime_numbers.py
@@ -6,11 +6,26 @@ Return a list of all the prime numbers inferior or equal to n
 def prime_numbers(n):
 
     def is_prime(n):
-        if n < 2:
+        """Returns True if n is prime."""
+        if n == 2:
+            return True
+        if n == 3:
+            return True
+        if n % 2 == 0:
             return False
-        for i in range(2,n):
-            if (n%i) == 0:
+        if n % 3 == 0:
+            return False
+
+        i = 5
+        w = 2
+
+        while i * i <= n:
+            if n % i == 0:
                 return False
+
+            i += w
+            w = 6 - w
+
         return True
 
     result = []
