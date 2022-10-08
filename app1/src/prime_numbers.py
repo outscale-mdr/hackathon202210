02a09b13428bd5596061f01656141341bb7943c2
Lhotte Louis <louis.lhotte@student-cs.fr>
2022-10-08 09:06:48 +0200

    Optimisation de sum_prime_numbers(n)

--- app1/src/prime_numbers.py
+++ app1/src/prime_numbers.py
@@ -28,4 +28,26 @@ Command: sum_prime_numbers
 Return a sum of all the prime numbers inferior or equal to n
 """
 def sum_prime_numbers(n):
-    return sum(prime_numbers(n))
+
+    S = 2
+
+    if n < 2:
+        return 0
+
+    prime = [True for _ in range(n + 1)]
+
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+
+    all_prime_numbers = [2]
+    for p in range(3, n, 2):
+        if prime[p]:
+            S += p
+            all_prime_numbers.append(p)
+    return S
+
+
