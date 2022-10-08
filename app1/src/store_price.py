cff27a3c8171d8e9bab6ae75ad5d026e85ab4cb3
JasonMendoza2008 <lhotteromain@gmail.com>
2022-10-08 16:29:19 +0200

    return str, 200 but convert to int

--- app1/src/store_price.py
+++ app1/src/store_price.py
@@ -2,9 +2,9 @@ import requests
 
 def clone_product(product_id, new_product_id, coef):
     LIST_PRODUCT_URL = "http://ms1:8000/clone"
-    items = requests.get(url = f"{LIST_PRODUCT_URL}/{product_id}/{new_product_id}/{coef}").json()
+    items = requests.get(url = f"{LIST_PRODUCT_URL}/{product_id}/{new_product_id}/{coef}").text
 
-    return len(items)
+    return int(items)
 
 """
 Find the sum of items prices of a product
