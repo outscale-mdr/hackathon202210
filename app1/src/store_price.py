754a31d3bc3db9180b64e99b1e3add59a611691b
JasonMendoza2008 <lhotteromain@gmail.com>
2022-10-08 14:51:17 +0200

    Removed comments

--- app1/src/store_price.py
+++ app1/src/store_price.py
@@ -1,21 +1,5 @@
 import requests
 
-"""
-The table "product_items" in db1 contains items with the following fields (see db1/db_init.sql):
-    id SERIAL PRIMARY KEY,
-    product_id integer NOT NULL,
-    name varchar(1000) NOT NULL,
-    price numeric NOT NULL
-
-This method clones all items of one product and apply a coefficien to their prices.
-    @param product_id (int) - source product id
-    @param new_product_id (int) - target product id
-    @param coef (double): price multiplicator
-
-Communication with the database is processed via the ms1
-
-Return the number of clonned items
-"""
 def clone_product(product_id, new_product_id, coef):
     LIST_PRODUCT_URL = "http://ms1:8000/product_items"
     items = requests.get(url = f"{LIST_PRODUCT_URL}/{product_id}").json()
