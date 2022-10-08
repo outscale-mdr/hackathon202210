617170b7787abe68dc07c83d52d9d149f089278b
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-08 12:29:36 +0000

    Use primesieve binding

--- app1/src/prime_numbers.py
+++ app1/src/prime_numbers.py
@@ -1,51 +1,14 @@
+
+from pyprimesieve import primes, primes_sum
 """
 Command: prime_numbers
 Return a list of all the prime numbers inferior or equal to n
 """
-
 def prime_numbers(n):
-    if n < 2:
-        return []
-
-    prime = [True for _ in range(n + 1)]
-
-    p = 2
-    while p * p <= n:
-        if prime[p]:
-            for i in range(p * p, n + 1, p):
-                prime[i] = False
-        p += 1
-
-    all_prime_numbers = [2]
-    for p in range(3, n, 2):
-        if prime[p]:
-            all_prime_numbers.append(p)
-    return all_prime_numbers
-
-
+    return primes(n)
 """
 Command: sum_prime_numbers
 Return a sum of all the prime numbers inferior or equal to n
 """
 def sum_prime_numbers(n):
-
-    if n < 2:
-        return 0
-
-    S = 2
-
-    prime = [True for _ in range(n + 1)]
-
-    p = 2
-    while p * p <= n:
-        if prime[p]:
-            for i in range(p * p, n + 1, p):
-                prime[i] = False
-        p += 1
-
-    all_prime_numbers = [2]
-    for p in range(3, n, 2):
-        if prime[p]:
-            S += p
-            all_prime_numbers.append(p)
-    return S
+    return primes_sum(n)
